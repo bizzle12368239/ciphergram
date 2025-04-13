0dee2717889d07399b2a7a5d45799b08e6aa322e
bizzle12368239 <jamiebrown6@gmail.com>
2025-04-13 14:33:19 +0100

    Add redirects for specific routes in next.config.js
    - Implemented redirects for '/recroom', '/library', and '/prisoncell' to their respective new paths under '/ironwood'.
    - Ensured all redirects are marked as permanent for SEO benefits.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  async redirects() {
+    return [
+      {
+        source: '/recroom',
+        destination: '/ironwood/recroom',
+        permanent: true,
+      },
+      {
+        source: '/library',
+        destination: '/ironwood/library',
+        permanent: true,
+      },
+      {
+        source: '/prisoncell',
+        destination: '/ironwood/prisoncell',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
